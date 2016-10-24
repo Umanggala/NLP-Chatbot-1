c6482122e77b3f4ea4c8fef6c3ae621d1d578c1f
dhaval08 <shahdhaval1893@gmail.com>
2016-10-23 19:57:28 -0700

    Initial GUI

---
+++ GUI/get_response.py
@@ -0,0 +1,5 @@
+def getReponse(user_query):
+    if user_query == "Good Morning":
+        return "Good morning, how are you doing today?"
+    else:
+        return "With pleasure"
---
+++ GUI/project_gui.py
@@ -0,0 +1,33 @@
+from tkinter import *
+import tkinter
+from get_response import *
+
+
+root = tkinter.Tk()
+frame = Frame(root)
+frame.pack()
+
+bottomframe = Frame(root)
+bottomframe.pack( side = BOTTOM )
+
+# Create the text field where user will enter his query
+v = StringVar()
+
+E1 = Entry(frame, bd =5, textvariable= v)
+E1.pack(side = LEFT)
+
+
+def buttonAction(user_query):
+
+    reply = getReponse(user_query)
+    var = StringVar()
+    label = Message( root, textvariable=var, relief=RAISED, width = 1000)
+
+    var.set(reply)
+    label.pack()
+
+B = tkinter.Button(frame, text ="Send", command = lambda:buttonAction(v.get()))
+B.pack(side = RIGHT)
+
+root.mainloop()
+
