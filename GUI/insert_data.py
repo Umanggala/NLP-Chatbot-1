3b2e43404e6ee5304ee467b58d328544095c2270
umanggala <umanggala9@gmail.com>
2016-11-04 15:09:56 -0700

    Database made

--- GUI/insert_data.py
+++ GUI/insert_data.py
@@ -1,36 +1,72 @@
 from pymongo import MongoClient
-from datetime import datetime
+
 
 client = MongoClient()
 db = client.test
-'''
-result = db.restaurants.insert_one(
-    {
-        "address": {
-            "street": "1119W",
-            "zipcode": "90007",
-            "building": "1",
-            "coord": [-73.9557413, 40.7720266]
-        },
-        "borough": "Manhattan",
-        "cuisine": "Italian",
-        "grades": [
+
+#Inserts the data from the file.
+#Format: restaurant_name <tab> place <tab> price_range <tab> cuisine
+
+def insert_data_from_text():
+
+    new_dict = {}
+    name = ""
+    place = ""
+    price_range = ""
+    cuisine = ""
+    count = 1
+
+    with open("/Users/umanggala/Desktop/Courses/NLP/NLP-Chatbot/restaurants.txt") as f:
+
+        for line in f:
+            splitLine = line.split("\t")
+            name = splitLine[0]
+            place = splitLine[1]
+            price_range = splitLine[2]
+            cuisine = splitLine[3]
+
+            new_dict[count] = [name, place, price_range, cuisine]
+            count += 1
+    f.close()
+
+    return new_dict
+
+#Inserts the data in the database
+def insert_into_database(new_dict):
+
+    result = db.restaurants.remove()
+
+    id = 41704620
+    for i in new_dict:
+        result = db.restaurants.insert_one(
             {
-                "date": datetime.strptime("2014-10-01", "%Y-%m-%d"),
-                "grade": "A",
-                "score": 11
-            },
-            {
-                "date": datetime.strptime("2014-01-16", "%Y-%m-%d"),
-                "grade": "B",
-                "score": 17
+                "address": {
+                    "street": new_dict.get(i)[1],
+                    "zipcode": "90007",
+                },
+                "cuisine": new_dict.get(i)[3].rstrip('\n'),
+                "name": new_dict.get(i)[0],
+                "restaurant_id": id,
+                "price_range": new_dict.get(i)[2]
             }
-        ],
-        "name": "Vella",
-        "restaurant_id": "41704620"
-    }
-)
-'''
-cursor = db.restaurants.find({"borough": "Manhattan"})
-for document in cursor:
-    print(document)
+        )
+        id += 1
+
+#Searches the name of the restaurants. Returns a dictionary containing the names of the restaurants.
+
+def search_data(search_cuisine,search_price_range,search_location):
+    cursor = db.restaurants.find({"cuisine":search_cuisine,"price_range":search_price_range,"address.street":search_location})
+    #cursor = db.restaurants.find({})
+    name_search = {}
+    count = 0
+    for document in cursor:
+        name_search[count] = document['name']
+        count += 1
+    return name_search
+
+
+restaurant = insert_data_from_text()
+insert_into_database(restaurant)
+
+restaurant_names = search_data("indian","cheap","artesia")
+print(restaurant_names)
